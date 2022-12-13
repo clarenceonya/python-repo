33d50fb58bc685766fff579a861a7135b80d6183
clarenceonya <clarenceechia@gmail.com>
2022-12-12 23:20:55 -0500

    important data

---
+++ variable.py
@@ -0,0 +1,28 @@
+x=10
+print(x)
+type(x)
+print(type(x)) # printing out the data type of variable x
+y=10.7
+type(y)
+print(type(y)) # printing out the data type of variable y
+x="clarence"
+print("clarence")
+print(type("clarence"))
+x="clarence"
+print(x)
+print(type(x))
+# x=10
+# y=20
+# z=30 
+(x,y,z)=10,20,30
+print(x,y,z)
+b=[5,10,20,30,40]
+print(b)
+print(type(b))
+print(b[2])
+print(b[3])
+r=["dog","pig","goat","cat","monkey","sheep"]
+print(r)
+print(type(r))
+print(r[2])
+print(r[5])
