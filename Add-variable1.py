6d160f400da35a9d877919335c4dbfad9ded24ab
clarenceonya <clarenceechia@gmail.com>
2022-12-14 22:36:28 -0500

    groupwork

--- Add-variable1.py
+++ Add-variable1.py
@@ -1,2 +1,27 @@
 # store input numbers from the user
-a = input()
+a = input("enter the first number: ")
+b = input("enter the second number: ")
+
+# add the numbers
+sum = float(a)+float(b)
+
+# print results
+print("the sum of {0} and {1} is {2}".format(a,b,sum))
+
+# multiply the numbers 
+multiply = float(a)*float(b)
+
+# print results
+print("the multiplication of {0} and {1} is {2}".format(a,b,multiply))
+
+# divide the numbers
+divide = float(a)/float(b)
+
+# print results
+print("the division of {0} and {1} is {2}".format(a,b,divide))
+
+# subtract the numbers
+subtract = float(a)-float(b)
+
+# print results
+print("the subtraction of {0} and {1} is {2}".format(a,b,subtract))
---
+++ hello.py
@@ -0,0 +1,7 @@
+# store input numbers from the user
+a = input("enter the first number: ")
+b = input("enter the second number: ")
+
+sum = float(a)+float(b)
+
+print("the sum of {0} and {1} is {2}".format(a,b,sum))
