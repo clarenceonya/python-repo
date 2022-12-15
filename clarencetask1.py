90ec4217fc872674a00dfb7ea2f3e2e27f3848f1
clarenceonya <clarenceechia@gmail.com>
2022-12-14 21:27:49 -0500

    classwork1

---
+++ clarencetask1.py
@@ -0,0 +1,24 @@
+d=20
+print(d)
+type(d)
+print(type(d))
+g="John"
+print(g)
+type(g)
+print(type(g))
+k=45.5
+print(k)
+type(k)
+print(type(k))
+# d=20
+# g="john"
+# k=45.5
+(d,g,k)=20,"John",45.5
+print(d,g,k)
+s=["samuel","relindis","clarence","thelma"]
+print(s)
+print(type(s))
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
---
+++ task2.py
@@ -0,0 +1,3 @@
+c=50
+d=10
+print(c+d,c-d,c*d,c/d)
